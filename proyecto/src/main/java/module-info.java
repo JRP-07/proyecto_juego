6c1f4deb60dcdd5af6b9d08a09a83f374e4b1bd9
Rafa <jose.rperezg07@gmail.com>
2026-06-21 16:13:56 -0600

    quitar carpetas innecesarias

--- proyecto/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module jrp.progra.proyecto {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.base;
-    requires javafx.graphics;
-
-    opens jrp.progra.proyecto to javafx.fxml;
-    exports jrp.progra.proyecto;
-}
